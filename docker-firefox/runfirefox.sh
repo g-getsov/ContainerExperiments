e790624deeb1c40a13aa09677593753d1db92570
Georgi Getsov <getsov.g@gmail.com>
2017-12-02 23:59:14 +0000

    fix

--- docker-firefox/runfirefox.sh
+++ docker-firefox/runfirefox.sh
@@ -3,5 +3,5 @@ docker run -ti --rm \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -v $HOME/.Xauthority:/home/developer/.Xauthority \
 -v /dev/snd:/dev/snd \
--v /run/user/$UID/pulse/native:/home/developer/pulse/socket
+-v /run/user/$UID/pulse/native:/home/developer/pulse/socket \
 --net=host --pid=host --ipc=host firefox
