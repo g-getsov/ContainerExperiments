7f72b8fdc10f4645647ea216779c5d1dc2a6e152
Georgi Getsov <getsov.g@gmail.com>
2017-12-02 23:00:10 +0000

    initial commit

---
+++ docker-firefox/runfirefox.sh
@@ -0,0 +1,7 @@
+docker run -ti --rm \
+-e DISPLAY \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v $HOME/.Xauthority:/home/developer/.Xauthority \
+-v /dev/snd:/dev/snd \
+-v /run/user/$UID/pulse/native:/home/developer/pulse/socket
+--net=host --pid=host --ipc=host firefox
