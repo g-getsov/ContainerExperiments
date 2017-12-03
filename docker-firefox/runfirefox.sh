4307048c51755c000244a7797d999ccc40aa6b3b
Georgi Getsov <getsov.g@gmail.com>
2017-12-03 17:58:31 +0000

    made x server and sound sockets read only

--- docker-firefox/runfirefox.sh
+++ docker-firefox/runfirefox.sh
@@ -1,7 +1,7 @@
 docker run -ti --rm \
 -e DISPLAY \
--v /tmp/.X11-unix:/tmp/.X11-unix \
--v $HOME/.Xauthority:/home/developer/.Xauthority \
+-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+-v $HOME/.Xauthority:/home/developer/.Xauthority:ro \
 -v /dev/snd:/dev/snd \
--v /run/user/$UID/pulse/native:/home/developer/pulse/socket \
+-v /run/user/$UID/pulse/native:/home/developer/pulse/socket:ro \
 --net=host --pid=host --ipc=host firefox
