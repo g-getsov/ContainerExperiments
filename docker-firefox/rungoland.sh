2ab56653dfa59123c73fd03184d279e13a222227
Georgi Getsov <getsov.g@gmail.com>
2017-12-03 18:10:23 +0000

    made sockets writable

--- docker-firefox/rungoland.sh
+++ docker-firefox/rungoland.sh
@@ -1,9 +1,9 @@
 docker run -ti --rm \
 -e DISPLAY \
--v /tmp/.X11-unix:/tmp/.X11-unix \
--v $HOME/.Xauthority:/home/developer/.Xauthority \
--v /dev/snd:/dev/snd \
--v /run/user/$UID/pulse/native:/home/developer/pulse/socket \
+-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+-v $HOME/.Xauthority:/home/developer/.Xauthority:ro \
+-v /dev/snd:/dev/snd:ro \
+-v /run/user/$UID/pulse/native:/home/developer/pulse/socket:ro \
 -v /home/anonymous/Programs/goland:/home/developer/goland \
 -v /home/anonymous/Workspace:/home/developer/Workspace \
 --net=host --pid=host --ipc=host goland 
